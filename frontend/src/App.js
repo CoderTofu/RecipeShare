cb185a11313e15fb875ff5deee866b7cb34023b5
JPaolo <vioneverforget@gmail.com>
2023-03-10 11:05:24 +0800

    did progress on home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,47 +15,29 @@ import Recipe from "./pages/recipe";
 export default function App() {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand navbar-brand-lg">RecipeShare</Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <div className="navbar-container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-transparent p-4 mx-5">
+          <Link className="navbar-brand navbar-brand-lg">RecipeShare</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link">Recipes</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link">Create</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      {/* <header className="navbar">
-
-        <h1 className="">
-          <Link className="navbar-brand site-name" to="/">RecipeShare</Link>
-        </h1>
-
-        <nav className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to='/'>Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/recipes'>Recipes</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/create'>Create</Link>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to='/' className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/recipes' className="nav-link">Recipes</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/create' className="nav-link">Create</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
-
-      </header> */}
+      </div>
+      
 
       <Routes>
         <Route exact path='/' element={<Home />} />
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+import "../css/home.css"
+
 export default function Home() {
     return (
-        <div>
-            This is a home page
+        <div className="mx-5 p-4">
+            <div className="landing img-frame">
+                <div className="site-headings">
+                    <h3 className="creator-header">CoderTofu</h3>
+                    <h1 className="brand-header">RecipeShare</h1>
+                    <div className="slogan-container">
+                        <p className="brand-slogan">Sharing recipes, sharing happiness</p>
+                    </div>
+                    <button><Link to='/recipes'>Learn more</Link></button>
+                </div>
+            </div>
         </div>
     )
 }
